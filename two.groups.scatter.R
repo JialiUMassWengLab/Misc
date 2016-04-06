a771dd6512c2c6774b9684e90de34ae1f1652322
Zhuang <zhuangj@ummsres18.umassmed.edu>
2016-04-06 16:52:49 -0400

    20160406 modify two.groups.scatter.R

--- two.groups.scatter.R
+++ two.groups.scatter.R
@@ -7,6 +7,6 @@ for (x in 1:nrow(data)) {
     plot(as.numeric(data[x,33:63])+perturbation,as.numeric(data[x,2:32]),type="n",ylab="RPKM",xaxt="n",xlab="",main=data[x,1],xlim=c(-0.5,1.5))
 #    axis(1, labels = FALSE)
     text(cex=0.9, x=c(0.15,1.05), y=par("usr")[3]-0.04*(par("usr")[4]-par("usr")[3]), adj=c(1,1.2), c("No SV","SV"), xpd=TRUE)
-    for (y in 2:32) lines(data[x,y+31]+perturbation[y-1],data[x,y],type="p",pch=19,col=ifelse(data[x,y+31]==1,"red","black"))
+    for (y in 2:32) lines(data[x,y+31]+perturbation[y-1],data[x,y],type="p",pch=21,col="white",bg=ifelse(data[x,y+31]==1,"red","black"),cex=1.2,lwd=0.8)
 }
 dev.off()
